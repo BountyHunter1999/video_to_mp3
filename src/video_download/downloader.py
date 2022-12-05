56e5529a41e9f2d813bc2767cd640a61ca44f4d1
mikeyy2 <adhikari.shrish1@gmail.com>
2022-12-05 21:20:15 +0545

    Video downloading feature added

--- src/video_download/downloader.py
+++ src/video_download/downloader.py
@@ -14,8 +14,8 @@ class MyLogger(object):
 
 
 def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
+    if d["status"] == "finished":
+        print("Done downloading, now converting ...")
 
 
 ydl_opts = {
@@ -25,43 +25,74 @@ ydl_opts = {
     #     'preferredcodec': 'mp3',
     #     'preferredquality': '192',
     # }],
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
+    "logger": MyLogger(),
+    "progress_hooks": [my_hook],
 }
 
 import concurrent.futures
 
+
 def download(url):
     if type(url) is not list:
-        url = [url] 
+        url = [url]
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(url)
 
+
+def download_with_ydl(url, ydl):
+    if type(url) is not list:
+        url = [url]
+    ydl.download(url)
 
 
 urls = [
     "https://youtu.be/rUWxSEwctFU",
     "https://youtu.be/UT5F9AXjwhg",
     "https://youtu.be/lTTajzrSkCw",
-    "https://youtu.be/cwjMwmDSKV0"
+    "https://youtu.be/cwjMwmDSKV0",
 ]
 
 import time
 import os
+import threading
 
-if __name__ == "__main__":
-    start_time = time.time()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            executor.map(download, urls)
-    duration = start_time - time.time()
-    print(f"Downloaded {len(urls)} in {duration} seconds with threading")
-    
+def start_clean():
     for i in os.listdir():
         if i.endswith("mp4"):
             os.remove(i)
+
+if __name__ == "__main__":
+    start_clean()
+    start_time = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download, urls)
+    duration = time.time() - start_time
+    print(f"Downloaded {len(urls)} in {duration} seconds with threading")
+    # Downloaded 4 in 59.12602138519287 seconds with threading
+
+    start_clean()
+
+    print()
     print("Direct download")
     start_time = time.time()
     download(urls)
-    duration = start_time - time.time()
+    duration = time.time() - start_time
     print(f"Downloaded {len(urls)} in {duration} seconds without threading")
+    # Downloaded 4 in 111.34024572372437 seconds without threading
 
+    start_clean()
+
+    print()
+    thread_list = []
+    start_time = time.time()
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("download with ydl as root context")
+        for url in urls:
+            worker = threading.Thread(target=download_with_ydl, args=(url, ydl))
+            worker.start()
+            thread_list.append(worker)
+        for worker in thread_list:
+            worker.join()
+    duration = time.time() - start_time
+    print(f"Downloaded {len(urls)} in {duration} seconds ydl as root context")
+    # Downloaded 4 in 59.3468132019043 seconds ydl as root context
